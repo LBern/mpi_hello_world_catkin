a75c9644ee75109892a5978206b5cfa9290fcc3e
lbern <LBern@users.noreply.github.com>
2021-12-20 11:38:04 +0100

    Delete hello-world.h

--- include/mpi_hello_world/hello-world.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_MPI_HELLO_WORLD_HELLO_WORLD_H_
-#define INCLUDE_MPI_HELLO_WORLD_HELLO_WORLD_H_
-
-#include <string>
-
-std::string hello_world_cu();
-
-#endif  // INCLUDE_MPI_HELLO_WORLD_HELLO_WORLD_H_
